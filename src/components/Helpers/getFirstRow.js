b312256daa447b32548769ea660d314f3cbc8c4c
Nawaz Pasha <nawaz22022001@gmail.com>
2022-12-30 06:28:32 +0530

    Helper Functions

---
+++ src/components/Helpers/getFirstRow.js
@@ -0,0 +1,36 @@
+let firstItems = []
+
+const getNoofRows = (totalLength, rowLength) => {
+    return totalLength / rowLength
+}
+
+const generateRowFirstItem = (rowLength, totalLength) => {
+    let arr = []
+    let rows = getNoofRows(totalLength, rowLength)
+
+    let item = 1
+    for (let i = 0; i < rows; i++) {
+        arr.push(item)
+        item = item + rowLength
+    }
+    firstItems = arr
+}
+
+const getFirstItemofRow = (number, rowLength, totalLength) => {
+    generateRowFirstItem(rowLength, totalLength)
+    let value = 0;
+    firstItems.map((item) => {
+        if (number > item) {
+            value = item
+        } else if (number == item) {
+            console.log(number, item);
+            value = number
+        }
+        else {
+            value = value
+        }
+    })
+    return value
+}
+
+export default getFirstItemofRow
