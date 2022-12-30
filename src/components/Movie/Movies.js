cef7c64737a68b5ff5eb854d3bcacdf2eb1090fe
Nawaz Pasha <nawaz22022001@gmail.com>
2022-12-30 06:25:42 +0530

    Movie Component

---
+++ src/components/Movie/Movies.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useState } from 'react'
+import movieStyle from './Movie.module.css'
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import NoImage from '../../assets/No-Image.png'
+
+
+const Movie = ({ name, ImageUrl, id, handleClick, movies, TrailerURL }) => {
+    const [img, setImg] = useState(ImageUrl)
+
+    const handleError = (err) => {
+        setImg(NoImage)
+    }
+
+    return (
+        <>
+            <div className={movieStyle.main} onClick={() => handleClick({ name, ImageUrl, id, movies, TrailerURL })}>
+                {img ? <img src={img} onError={handleError}></img> : undefined}
+                <span className={movieStyle.name}>{name}</span>
+                {(!img) ? <PlayCircleOutlineIcon className={movieStyle.icon} /> : undefined}
+            </div>
+        </>
+    )
+}
+
+export default Movie
